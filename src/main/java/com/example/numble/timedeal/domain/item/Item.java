16d765b87d7bd62fab12cd127ec99ccb34090a01
dev1week <rondo2860@gmail.com>
2023-03-02 22:22:41 +0900

    #8 [1] Item 도메인 수정
    -비즈니스 로직으로 재고의 증감을 나타내는 메서드 작성 
    - 재고 수량이 부족할 경우 예외를 던지도록 예외처리

--- src/main/java/com/example/numble/timedeal/domain/item/Item.java
+++ src/main/java/com/example/numble/timedeal/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.numble.timedeal.domain.item;
 
 import com.example.numble.timedeal.domain.Category;
+import com.example.numble.timedeal.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,24 @@ public abstract class Item {
     private int stockQuantity;
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //엔티티 먼으로 비즈니스 로직을 해결할 수 있다면 엔티티 안에 넣는 것이 좋다.
+
+    //재고 수량 증가 buisness logic
+    public void addStock(int quantity){this.stockQuantity += quantity;}
+
+    //재고 수량 감소 buisness logic
+    public void discriminateStock(int quantity){
+        int rest = this.stockQuantity - quantity;
+        validateRest(rest);
+        this.stockQuantity = rest;
     }
+
+    //재고 수량아 0 밑으로 내려갈 경우 에러 반환
+    private void validateRest(int rest){
+        if(rest<0){
+            throw new NotEnoughStockException("재고 수량이 부족합니다.");
+        }
+    }
+}
 
