4e6b8dbd17c76dafec4834ec09ed5c12796f52c8
dev1week <rondo2860@gmail.com>
2023-03-03 14:32:19 +0900

    #10 [4] 검색 테스트 정의

--- src/test/java/com/example/numble/timedeal/service/OrderServiceTest.java
+++ src/test/java/com/example/numble/timedeal/service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import com.example.numble.timedeal.domain.Order;
 import com.example.numble.timedeal.domain.OrderStatus;
 import com.example.numble.timedeal.domain.item.Book;
 import com.example.numble.timedeal.domain.item.Item;
+import com.example.numble.timedeal.repository.OrderSearch;
 import com.example.numble.timedeal.exception.NotEnoughStockException;
 import com.example.numble.timedeal.repository.OrderRepository;
 import org.junit.Assert;
@@ -18,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +33,55 @@ public class OrderServiceTest {
     @Autowired OrderService orderService;
     @Autowired
     OrderRepository orderRepository;
+
+    @Test
+    public void 주문검색() throws Exception{
+        //given
+        Member member = createMember("test");
+        Item item = createBook("book", 10000, 10);
+        Item item2 = createBook("book2", 10000, 10);
+        int orderCount =2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        Long orderId2 = orderService.order(member.getId(), item2.getId(), orderCount);
+        orderService.cancelOrder(orderId2);
+
+
+        //when
+        OrderSearch givenNothing = new OrderSearch();
+
+        OrderSearch givenName = new OrderSearch();
+        givenName.setMemberName("test");
+
+        OrderSearch givenTypeOrder = new OrderSearch();
+        givenTypeOrder.setOrderStatus(OrderStatus.ORDER);
+
+        OrderSearch givenTypeCancel = new OrderSearch();
+        givenTypeCancel.setOrderStatus(OrderStatus.CANCEL);
+
+        OrderSearch givenBoth = new OrderSearch();
+        givenBoth.setMemberName("test");
+        givenBoth.setOrderStatus(OrderStatus.ORDER);
+
+        //then
+        List<Order> resultGivenNothing = orderService.findOrders(givenNothing);
+        Assert.assertEquals(2, resultGivenNothing.size());
+
+        List<Order> resultGivenTypeCancel = orderService.findOrders(givenTypeCancel);
+        Assert.assertEquals(1, resultGivenTypeCancel.size());
+        Assert.assertEquals(orderId2, resultGivenTypeCancel.get(0).getId());
+
+        List<Order> resultGivenTypeOrder = orderService.findOrders(givenTypeOrder);
+        Assert.assertEquals(1, resultGivenTypeOrder.size());
+        Assert.assertEquals(orderId, resultGivenTypeOrder.get(0).getId());
+
+        List<Order> resultGivenName = orderService.findOrders(givenName);
+        Assert.assertEquals(2, resultGivenName.size());
+
+        List<Order> resultGivenBoth = orderService.findOrders(givenBoth);
+        Assert.assertEquals(1, resultGivenBoth.size());
+        Assert.assertEquals(orderId, resultGivenBoth.get(0).getId());
+
+    }
 
     @Test
     public void 상품주문() throws Exception{
