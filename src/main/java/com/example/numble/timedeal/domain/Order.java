463ae6cc6d7f1c38284000c2dd1525f14cc1b7cc
dev1week <rondo2860@gmail.com>
2023-03-03 13:33:34 +0900

    #10 [2] domain으로만 비즈니스 로직이 작성 가능할 경우 작성
    - order 
    1. 생성 메서드 추가 
    2. 주문 취소 메서드 추가
    3.  가격총합 조회 메서드 추가 
    
    -orderitem 
    1. 생성 메서드 추가 
    2. 주문 취소 메서드 추가
    3. 주문 가격 총액 조회 메서드 추가

--- src/main/java/com/example/numble/timedeal/domain/Order.java
+++ src/main/java/com/example/numble/timedeal/domain/Order.java
@@ -1,6 +1,8 @@
 package com.example.numble.timedeal.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
     @Id
     @GeneratedValue
@@ -25,5 +28,47 @@ public class Order {
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
 
-
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
     }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+
+    //생성
+    //오더 테이블이 member와 order_item 테이블과 연관관계를 가지고 있기 때문에 생성시, 한번에 만들도록 메서드를 만들었다.
+    public static Order createOrder(Member member, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //주문취소
+    public void cancel(){
+        this.setStatus(OrderStatus.CANCEL);
+        //재고를 원래대로 되돌린다.
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //가격총합조회
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice +=orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
+
+}
